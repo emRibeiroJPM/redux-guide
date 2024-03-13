7ccb1a7967e699ea329d7556c8a0f29f4fddc9fe
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-13 03:07:15 +0000

    feat:add user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,22 +2,39 @@ import { useState } from "react";
 
 // Components
 import Cart from "../cart/index";
+import { useSelector, useDispatch } from "react-redux";
 
 // Styles
 import * as Styles from "./styles";
+import { UserActionTypes } from "../../redux/user/action-types";
+import { userLogin, userLogout } from "../../redux/user/actions";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const dispachar = useDispatch();
+  console.log({ currentUser });
 
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+    dispachar(userLogin({ user: "Emanuel", email: "emanribeiro13@gmail.com" }));
+  };
+  const handleLogoutClick = () => {
+    dispachar(userLogout());
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser != null ? (
+          <div onClick={handleLogoutClick}>Sair</div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
